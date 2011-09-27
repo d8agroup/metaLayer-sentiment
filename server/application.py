d1f200923a6cf79bdf30ba9b6f3ebc9638363d24
Matthew Griffiths <mrmatthewgriffiths@gmail.com>
2011-09-27 14:13:00 -0400

    Added sentiment

--- server/application.py
+++ server/application.py
@@ -1,8 +1,28 @@
 from flask import Flask, request, jsonify
 from configuration import *
+import math
+import re
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 app = Flask(__name__)
 
-@app.route('/test')
-def test():
-    return "worked 2"
+@app.route('/getsentiment', methods=['POST'])
+def sentiment_analysis():
+    if 'text' not in request.form:
+        return jsonify(ERROR_NOTEXT)
+    
+    file = open(AFINN_FILE, 'r')
+    sentiment_dict = dict(map(lambda (w, s): (w, int(s)), [ws.strip().split('\t') for ws in file]))
+    file.close()
+    pattern_split = re.compile(r"\W+")
+    words = pattern_split.split(request.form['text'].lower())
+    sentiments = map(lambda word: sentiment_dict.get(word, 0), words)
+    if sentiments:
+        # How should you weight the individual word sentiments?
+        # You could do N, sqrt(N) or 1 for example. Here I use sqrt(N)
+        sentiment = float(sum(sentiments))/math.sqrt(len(sentiments))
+    else:
+        sentiment = 0
+    return jsonify(status='success', sentiment=sentiment)
--- server/configuration.py
+++ server/configuration.py
@@ -2,6 +2,10 @@ import socket
 import sys
 
 MASK_ERRORS = True
+AFINN_FILE = '/usr/local/metaLayer-sentiment/resources/AFINN.txt'
+
+ERROR_NOTEXT = {'status':'failed', 'code':101, 'error':'The required POST field \'text\' was not supplied' }
 
 if socket.gethostname() == 'matt-griffiths':
     MASK_ERRORS = False
+    AFINN_FILE = '/home/matt/code/metaLayer/sentiment/resources/AFINN.txt'
